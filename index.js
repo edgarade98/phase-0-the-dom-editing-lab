478d3c22c2b791c26a49be48faa7ef5f19849b20
Edgar <eddyade98@gmail.com>
2023-09-27 10:39:38 +0300

    Completed Assignment

---
+++ index.js
@@ -0,0 +1,23 @@
+
+document.querySelector('html'); 
+const element = document.getElementsByTagName ('body');
+
+document.querySelector('body'); {
+const header = document.getElementsByTagName ('h1');
+
+const p = document.getElementsByTagName ('p');
+
+const table = document.getElementsByTagName ('table');
+
+}
+
+document.querySelector('p'); {
+   const strong = document.getElementsByTagName ('strong');
+
+   const em = document.getElementsByTagName ('em');
+
+   const a = document.getElementsByTagName ('a');
+}
+
+
+
